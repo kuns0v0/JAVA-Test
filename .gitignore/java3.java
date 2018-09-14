c67fae6b1cbbefbcbe9bb7325611bdd6a2ced4ee
kuns0v0 <43167963+kuns0v0@users.noreply.github.com>
2018-09-14 23:11:16 +0800

    Add files via upload
    测试数组溢出

---
+++ .gitignore/java3.java
@@ -0,0 +1,7 @@
+public class java3 {
+    public static void main(String []args) {
+		int array[]={1,2,3,4,5};
+		System.out.println("array[1]= "+array[1]);
+		System.out.println("array[-1]= "+array[-1]);
+    }
+}
